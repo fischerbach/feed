1ddca3d724eb51b75b101409e5da4cc08f2288f4
fischerbach <rafalrybnik@yahoo.com>
2023-09-07 22:24:57 +0200

    w

---
+++ dashboard.py
@@ -0,0 +1,42 @@
+import streamlit as st
+import pandas as pd
+
+import requests
+
+import json
+
+
+url = "https://totalbet.pl/rest/market/categories"
+
+
+payload={}
+headers = {}
+
+response = requests.request("GET", url, headers=headers, data=payload)
+
+categories = pd.DataFrame(response.json()['data'])
+# st.write(categories)
+
+category = st.selectbox("Wybierz kategorię", categories['categoryName'])
+
+if category:
+	category_id = categories.query(f"`categoryName` == '{category}'").values[0][0]
+	# st.write(category_id)
+	url = f"https://totalbet.pl/rest/market/categories/{category_id}/events"
+	response = requests.request("GET", url, headers=headers, data=payload)
+	events = pd.DataFrame(response.json()['data'])
+	events_choose = st.multiselect("Wybierz wydarzenia", events['eventName'])
+
+	if(events_choose):
+			
+		# st.write(events_choose)
+		events.query(f"eventName in {events_choose}", inplace=True)
+		# st.write(events)
+
+		feed = response.json()['data']
+		def filter_feed (event):
+			return event['eventName'] in events_choose
+		feed = list(filter(filter_feed, feed))
+		# st.write(json.dumps(feed))
+		st.header("Feed")
+		st.markdown(f"<textarea rows='10' style='width:100%;'>var feed = {feed}</textarea>", unsafe_allow_html=True)
